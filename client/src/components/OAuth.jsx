8ff2e9eded4bbb5b3ff125ac0f336289f9ea3322
Cris <cckaiser2563@gmail.com>
2024-05-27 23:44:14 +0200

    Added Google sign on button

---
+++ client/src/components/OAuth.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function OAuth() {
+    const handleGoogleClick = async () => {
+        try{
+
+        }catch (error){
+            console.log("Could not sigh in with google",  error)
+        }
+    }
+  return (
+    <button onClick={handleGoogleClick} type="button" className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
+        Continue with google
+    </button>
+  )
+}
